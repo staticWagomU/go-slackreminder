c87884807017400f86c8030f43882706fea9f3b3
staticWagomU <to6wa17@gmail.com>
2024-12-03 23:05:26 +0900

    feat: 送信先を選択するprompt

---
+++ cmd/chooseDestination.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/manifoldco/promptui"
+)
+
+func chooseDestination() (string, error) {
+	defaultDestination := "@me"
+	prompt := promptui.Prompt{
+		Label:   "送信先を入力してください。デフォルトは" + defaultDestination + "です。",
+		Default: defaultDestination,
+		Templates: &promptui.PromptTemplates{
+			Prompt:  "{{ . }} ",
+			Valid:   "{{ . | green }} ",
+			Invalid: "{{ . | red }} ",
+			Success: "{{ . | bold }} ",
+		},
+	}
+
+	result, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return "", err
+	}
+	if result == defaultDestination {
+		result = ""
+	}
+
+	return result, nil
+}
--- cmd/root.go
+++ cmd/root.go
@@ -8,17 +8,25 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use: "slack-reminder",
+	Use:   "slack-reminder",
 	Short: "Slackの`/remind`コマンドをインタラクティブに生成するCLIツール",
-	Long: "Slackの`/remind`コマンドをインタラクティブに生成するCLIツール。",
+	Long:  "Slackの`/remind`コマンドをインタラクティブに生成するCLIツール。",
+	Run: func(cmd *cobra.Command, args []string) {
+		GenerateReminderCommand()
+	},
 }
-
-
 
 func Execute() {
-  if err := rootCmd.Execute(); err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
+func GenerateReminderCommand() {
+	chosenDestination, err := chooseDestination()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
