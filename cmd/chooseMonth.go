abd6915daf7f75c9d10bf794817a298d6782421d
staticWagomU <to6wa17@gmail.com>
2024-12-06 21:55:49 +0900

    feat: 月を選択するプロンプト

---
+++ cmd/chooseMonth.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"log"
+
+	"github.com/charmbracelet/huh"
+)
+
+func ChooseMonth() (string, error) {
+	var command string
+
+	err := huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[string]().
+				Title("リマインドする月は？").
+				Options(
+					huh.NewOption("1月", "January"),
+					huh.NewOption("2月", "February"),
+					huh.NewOption("3月", "March"),
+					huh.NewOption("4月", "April"),
+					huh.NewOption("5月", "May"),
+					huh.NewOption("6月", "June"),
+					huh.NewOption("7月", "July"),
+					huh.NewOption("8月", "August"),
+					huh.NewOption("9月", "September"),
+					huh.NewOption("10月", "October"),
+					huh.NewOption("11月", "November"),
+					huh.NewOption("12月", "December"),
+				).
+				Value(&command),
+		),
+	).Run()
+
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+
+	return command, nil
+}
